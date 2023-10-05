3b5d5a03223b7fe6d8a10bc57349772977110283
Matthew MacDonald <matthew.macdonald@me.com>
2023-10-05 15:34:38 -0500

    TF Modules Commit

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "linux_vm_name" {
   value = values({for env, vm in azurerm_linux_virtual_machine.mylinuxvm: env => vm.name})
   sensitive = true
 }
-
-# output "linux_vm_id" {
-#   value = "azurerm_linux_virtual_machine.mylinuxvm[each.key].id"
-# }
-
